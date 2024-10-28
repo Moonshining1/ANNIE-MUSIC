535d4095e0d8e8758d1a375e07e7c74825db1ca9
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-28 18:58:01 +0530

    Add files via upload

---
+++ ANNIEMUSIC/__init__.py
@@ -0,0 +1,26 @@
+from ANNIEMUSIC.core.bot import MOON
+from ANNIEMUSIC.core.dir import dirr
+from ANNIEMUSIC.core.git import git
+from ANNIEMUSIC.core.userbot import Userbot
+from ANNIEMUSIC.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = MOON()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
