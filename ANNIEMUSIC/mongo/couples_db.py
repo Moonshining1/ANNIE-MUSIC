e65c066d1ee557f9d61b4d836b2c3d2cc33517f6
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-29 22:17:22 +0530

    Update couples_db.py

--- ANNIEMUSIC/mongo/couples_db.py
+++ ANNIEMUSIC/mongo/couples_db.py
@@ -1,36 +1,26 @@
-from ANNIEMUSIC.utils.mongo import db
+coupledb = {}
+# in memory storage
 
-coupledb = db.couple
 
 async def _get_lovers(cid: int):
-    lovers = await coupledb.find_one({"chat_id": cid})
-    if lovers:
-        lovers = lovers["couple"]
-    else:
-        lovers = {}
+    chat_data = coupledb.get(cid, {})
+    lovers = chat_data.get("couple", {})
     return lovers
 
-async def _get_image(cid: int):
-    lovers = await coupledb.find_one({"chat_id": cid})
-    if lovers:
-        lovers = lovers["img"]
-    else:
-        lovers = {}
-    return lovers
+
+async def get_image(cid: int):
+    chat_data = coupledb.get(cid, {})
+    image = chat_data.get("img", "")
+    return image
+
 
 async def get_couple(cid: int, date: str):
     lovers = await _get_lovers(cid)
-    if date in lovers:
-        return lovers[date]
-    else:
-        return False
+    return lovers.get(date, False)
 
 
 async def save_couple(cid: int, date: str, couple: dict, img: str):
-    lovers = await _get_lovers(cid)
-    lovers[date] = couple
-    await coupledb.update_one(
-        {"chat_id": cid},
-        {"$set": {"couple": lovers, "img": img}},
-        upsert=True,
-                              )
+    if cid not in coupledb:
+        coupledb[cid] = {"couple": {}, "img": ""}
+    coupledb[cid]["couple"][date] = couple
+    coupledb[cid]["img"] = img
