80de2e730947315a0661189bac8a88bf8678abfd
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-29 16:44:16 +0530

    Update mention.py

--- ANNIEMUSIC/plugins/admins/mention.py
+++ ANNIEMUSIC/plugins/admins/mention.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatType, ChatMemberStatus
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import ChatPermissions
 from ANNIEMUSIC import app
-from ANNIEMUSIC.utils.jarvis_ban import admin_filter
+from ANNIEMUSIC.utils.moon_ban import admin_filter
 
 
 
