20f7e113649c3e2b8e68a554b4ede89198c84541
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-29 18:50:36 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,11 +1,18 @@
-import re
-from os import getenv
+# Telegram API credentials - Get these from the Telegram API website
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
 
-from dotenv import load_dotenv
-from pyrogram import filters
-
-load_dotenv()
-
+# Specify where to get the following credentials
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Moonshining2")
+BOT_USERNAME = getenv("BOT_USERNAME", "musicXanime_bot")
+BOT_NAME = getenv("BOT_NAME", "Annie 𐌑ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊")
+ASSUSERNAME = getenv("ASSUSERNAME", "AnnieMusicPlayer")
+EVALOP = list(map(int, getenv("EVALOP", "7171698135").split()))
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+LOGGER_ID = int(getenv("LOGGER_ID", -1002024032988))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
+# External APIs - Get these from their respective providers
 CLEANMODE_DELETE_MINS = int(
     getenv("CLEANMODE_MINS", "5")
 )  # Remember to give value in Seconds
@@ -13,6 +20,54 @@ CLEANMODE_DELETE_MINS = int(
 DURATION_LIMIT_MIN = int(
     getenv("DURATION_LIMIT", "3000")
 )  # Remember to give value in Minutes
+GPT_API = getenv("GPT_API")
+DEEP_API = getenv("DEEP_API")
+OWNER_ID = int(getenv("OWNER_ID", 7297381612))
+
+# Heroku deployment settings - Refer to Heroku documentation on how to obtain these
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Moonshining1/ANNIE-MUSIC")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+GIT_TOKEN = getenv("GIT_TOKEN", None)
+
+# Support and contact information - Provide your own support channels
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Kittyxupdates")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Grandxmasti")
+
+# Set this to True if you want the assistant to automatically leave chats after an interval
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+
+# Server limits and configurations - These can be set based on your server configurations
+SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "2500"))
+SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", "9999999"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "9999999"))
+
+# External service credentials - Obtain these from Spotify
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "22b6125bfe224587b722d6815002db2b")
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "c9c63c6fbf2f467c8bc68624851e9773")
+
+# Telegram file size limits - Set these according to your requirements
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "5242880000"))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
+
+# Pyrogram session strings - You need to generate these yourself
+STRING1 = getenv("STRING_SESSION", None)
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+
+# Bot introduction messages - These can be customized as per your preference
+AYU = [
+    "💞", "🦋", "🔍", "🧪", "🦋", "⚡️", "🔥", "🦋", "🎩", "🌈", "🍷", "🥂", "🦋", "🥃", "🥤", "🕊️",
+    "🦋", "🦋", "🕊️", "🦋", "🕊️", "🦋", "🦋", "🦋", "🪄", "💌", "🦋", "🦋", "🧨"
+]
+
+AYUV = [
+    "╭───────────────────⦿\n│🐬 ▸ ʜᴇʏ, {0}\n│🌙 ▸ ɪ'ᴍ {1}\n├───────────────────⦿\n│☄️ ▸ ɪ ʜᴀᴠᴇ sᴘᴇᴄɪᴀʟ ғᴇᴀᴛᴜʀᴇs\n│✨ ▸ ᴀʟʟ-ɪɴ-ᴏɴᴇ ʙᴏᴛ\n├───────────────────⦿\n│🌴 ▸ ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs\n│💫 ▸ ᴄʜᴀᴛ-ʙᴏᴛ + ᴍᴜsɪᴄ-ʙᴏᴛ\n│💤 ▸ ʏᴏᴜ ᴄᴀɴ ᴘʟᴀʏ ᴍᴜꜱɪᴄ + ᴠɪᴅᴇᴏ\n│🍁 ▸ ɢᴇɴᴇʀᴀᴛᴏʀ ɪᴍᴀɢᴇs + ᴛᴀɢ ᴀʟʟ\n│🌠 ▸ ᴡᴇʟᴄᴏᴍᴇ + ʟᴇғᴛ ɴᴏᴛɪᴄᴇ\n│🕳️ ▸ 24x7 ᴏɴʟɪɴᴇ sᴜᴘᴘᴏʀᴛ\n├───────────────────⦿\n│🍷 ᴛᴀᴘ ᴛᴏ ᴄᴏᴍᴍᴀɴᴅs ᴍʏ ᴅᴇᴀʀ\n│🍹 ᴍᴀᴅᴇ ʙʏ🪽 ➪ [🇲σ᭡፝֟ɳ🌙♡︎](https://t.me/Moonshining2)\n╰───────────────────⦿"
+]
 
 ### DONT TOUCH or EDIT codes after this line
 BANNED_USERS = filters.user()
@@ -20,10 +75,49 @@ YTDOWNLOADER = 1
 LOG = 2
 LOG_FILE_NAME = "ANNIElogs.txt"
 TEMP_DB_FOLDER = "tempdb"
+
 adminlist = {}
 lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
 chatstats = {}
 userstats = {}
 clean = {}
 
 autoclean = []
+
+START_IMG_URL = getenv(
+    "START_IMG_URL", "https://envs.sh/A-y.png"
+)
+PING_VID_URL = getenv(
+    "PING_VID_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+)
+PLAYLIST_IMG_URL = "https://telegra.ph/file/94e9eca3b0ec6e2dc6cd5.png"
+STATS_VID_URL = "https://telegra.ph/file/e2ab6106ace2e95862372.mp4"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/ef5bdba78c475a9e50d24.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/c8db17e1612487be13571.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/6a81d918bd5d44c646205.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/1470316a51382cc446fe1.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/06679f04da4b2fbbb12d0.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/06679f04da4b2fbbb12d0.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/06679f04da4b2fbbb12d0.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/06679f04da4b2fbbb12d0.jpg"
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
+if SUPPORT_CHANNEL:
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+        )
+
+if SUPPORT_CHAT:
+    if not re.match("(?:http|https)://", SUPPORT_CHAT):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
+)
