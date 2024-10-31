2765225c367b4d361611e0d43559b2b9eba825a2
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-31 09:13:01 +0530

    Update help.py

--- ANNIEMUSIC/plugins/bot/help.py
+++ ANNIEMUSIC/plugins/bot/help.py
@@ -97,7 +97,7 @@ def paginate_modules(page_n, module_dict, prefix, chat=None, close: bool = False
     return pairs
 
 
-@app.on_message(filters.command(HELP_COMMAND) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
@@ -143,7 +143,7 @@ async def helper_private(
             )
 
 
-@app.on_message(filters.command(HELP_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
