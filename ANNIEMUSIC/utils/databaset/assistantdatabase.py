ad282c9ddc493052c926918bb40bf90645e02446
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-11-02 11:10:03 +0530

    Update assistantdatabase.py

--- ANNIEMUSIC/utils/databaset/assistantdatabase.py
+++ ANNIEMUSIC/utils/databaset/assistantdatabase.py
@@ -31,7 +31,7 @@ async def save_assistant(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from VIPMUSIC.core.userbot import assistants
+    from ANNIEMUSIC.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -45,7 +45,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from VIPMUSIC.core.userbot import assistants
+    from ANNIEMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -72,7 +72,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from VIPMUSIC.core.userbot import assistants
+    from ANNIEMUSIC.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -85,7 +85,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from VIPMUSIC.core.userbot import assistants
+    from ANNIEMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
